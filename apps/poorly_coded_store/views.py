c20dff8a457121d56f17b3983fad5f9c542443d0
Rashid Jama <rashidjama5@gmail.com>
2020-07-11 14:53:05 -0700

    amadon-assignement

--- apps/poorly_coded_store/views.py
+++ apps/poorly_coded_store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Order, Product
 
 def index(request):
@@ -8,9 +8,22 @@ def index(request):
     return render(request, "store/index.html", context)
 
 def checkout(request):
+    context = {
+        'orders': request.session['qt'],
+        'price': request.session['pr'],
+        'total': request.session['tp']
+    }
+    return render(request, 'store/checkout.html', context)
+
+def purchase(request):
+    my_price = Product.objects.get(id=request.POST['price'])
     quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
+    price_from_form = float( my_price.price)
     total_charge = quantity_from_form * price_from_form
-    print("Charging credit card...")
     Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+
+
+    request.session['qt'] = quantity_from_form
+    request.session['pr'] =   price_from_form
+    request.session['tp'] = total_charge
+    return redirect('checkout')
